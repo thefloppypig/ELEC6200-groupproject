d546cd52abed301df03b30f9fb45d8b732544684
Iván Durán Pérez <ivanduran0@gmail.com>
2021-11-12 13:49:17 +0000

    Changes in main

--- Deployment Software/main.py
+++ Deployment Software/main.py
@@ -68,34 +68,51 @@ def main(sensors_connected=True):
     'All_Cores']
 
     # Load the sensors as normal, only use fake sensors if specified
-    attack = "Normal"
-    bme280, i2c, tsl1, acc = None, None, None, None if not sensors_connected else initialiseSensors()
-    sensor_data_from_files, lines = None, Infinity 
     if (not sensors_connected): 
-        sensor_data_from_files, lines = loadFakeSensorData(attack,False)
+        attack = "Normal"
+        use_realdata = False
+        bme280, i2c, tsl1, acc = None, None, None, None
+        sensor_data_from_files, lines = loadFakeSensorData(attack, use_realdata)
+    else: 
+        bme280, i2c, tsl1, acc = initialiseSensors()
+        sensor_data_from_files, lines = None, Infinity 
+
+    #Load models
     models01, models11, attackClassificationModel = loadModels()
 
-    current_line = 0
-    sampling_rate = 10
-    true_positive = 0
+    #Sampling rates
+    sr_level_1 = 3 # Skipping lines from file
+    sr_level_2 = 1 # Skipping linesfrom file
+    
+    #Up in severity
+    no_attacks_1_to_2 = 3
+    no_attacks_2_to_attack = 6
+
+    #Down in severity
+    no_normal_1_to_sleep = 10
+    no_normal_2_to_1 = 4
+
+    #Counters
     round = 0
+    sleep_count = 0
+    current_line = 0
+    true_positive = 0
+    potential_attacks = 0
+
+    #Monitoring data (Unknown attacks)
     saved_results = []
     saved_sensor_data = []
-    potential_attacks = 0
-    level_1 = True
-    sleep_count = 0
+
     defense_triggered = ""
     while (current_line < lines):
         # Monitoring sensors begins
         start_time = time.time()
-        if potential_attacks < 3:
-            level_1 = True
-            time.sleep(3)
-            sampling_rate = 3
+        if potential_attacks < no_attacks_1_to_2:
+            time.sleep(sr_level_1)
+            sampling_rate = sr_level_1
         else:
-            level_1 = False
-            time.sleep(1)
-            sampling_rate = 1
+            time.sleep(sr_level_2)
+            sampling_rate = sr_level_2
         # print("-------------------------")
 
         # Get data from the sensors
@@ -122,24 +139,23 @@ def main(sensors_connected=True):
             potential_attacks = potential_attacks + 1
             sleep_count = 0
             reset_count = 0
-        elif not level_1: # If in level 2 and no attack detected
+        elif not (potential_attacks < no_attacks_1_to_2): # If in level 2 and no attack detected
             reset_count = reset_count + 1
             potential_attacks = 0
-            if reset_count > 4: # No attacks when in level 2 for a long time, reset to level 1
-                level_1 = True
+            if reset_count > no_normal_2_to_1: # No attacks when in level 2 for a long time, reset to level 1
                 sleep_count = 0
                 recover(defense_triggered)
-        elif level_1: # If in level 1 and no attack detected
+        elif potential_attacks < no_attacks_1_to_2: # If in level 1 and no attack detected
             sleep_count = sleep_count + 1
-            if sleep_count > 10:
+            if sleep_count > no_normal_1_to_sleep:
                 goToSleep()
                 return
 
         # Trigger Defense
-        if potential_attacks == 3:
+        if potential_attacks == no_attacks_1_to_2:
             defenseLevel1(return_data)
             defense_triggered = return_data
-        if potential_attacks == 4:
+        if potential_attacks == no_attacks_2_to_attack:
             defenseLevel2(return_data, saved_sensor_data, xOrder)
             return
 
